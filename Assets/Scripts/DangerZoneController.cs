beb06df6d5a402c0393394b33002e83a5235ba1c
Ekin Karıncalı <s230446019@stu.thk.edu.tr>
2026-04-07 17:37:38 +0300

    feat: Implement Ground Launcher system and connect to DangerZone trigger

--- Assets/Scripts/DangerZoneController.cs
+++ Assets/Scripts/DangerZoneController.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
-using System.Collections; // required for Coroutine
+using System.Collections;
 
 public class DangerZoneController : MonoBehaviour
 {
     [SerializeField] private FlightExamManager examManager;
     [SerializeField] private float missileDelay = 5f;
+
+    [SerializeField] private MissileLauncher launcher;
 
     private Coroutine activeCountdown;
 
@@ -18,7 +20,7 @@ public class DangerZoneController : MonoBehaviour
             
             // Start missile count down
             // it is a infrastucture for task 3
-            activeCountdown = StartCoroutine(MissileCountdownRoutine());
+            activeCountdown = StartCoroutine(MissileCountdownRoutine(other.transform));
         }
     }
 
@@ -37,17 +39,18 @@ public class DangerZoneController : MonoBehaviour
                 Debug.Log("Player escaped early. Countdown cancelled.");
             }
             
-            // TODO: Destroy the active missile (Task 3)
+            launcher.DestroyActiveMissile();
         }
     }
 
     // 5 second missile count down motor
-    private IEnumerator MissileCountdownRoutine()
+    private IEnumerator MissileCountdownRoutine(Transform targetTransform)
     {
         Debug.Log("Missile countdown started: 5 seconds...");
         yield return new WaitForSeconds(missileDelay);
         
         Debug.Log("LAUNCH MISSILE!");
-        // TODO: launch missile (Task 3)
+        
+        launcher.Launch(targetTransform);
     }
 }
